0d823a69f2d6915203f499a711d18027492b1b6d
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-01-19 10:13:15 +0500

    Add files via upload

---
+++ pkg.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const alertPKG = document.getElementById("prepaid");
+    alertPKG.addEventListener("click", () => {
+        alert("Your Sim is not prepaid! change your sim to prepaid to use these  packages");
+    })
+
+})  
